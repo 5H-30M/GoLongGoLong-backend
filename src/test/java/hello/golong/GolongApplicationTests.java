04204eb0e9f61a6977c905fd52f39fdcffe428d0
Haeun-Y <hahaha329@ewhain.net>
2023-03-08 00:03:59 +0900

    initial commit

---
+++ src/test/java/hello/golong/GolongApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.golong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
