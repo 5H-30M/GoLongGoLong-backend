cc5a719f162caf9769b561244870f536bbd26f16
DAHEEKIM1 <66730012+DAHEEKIM1@users.noreply.github.com>
2023-05-02 22:41:31 +0900

    [FEAT]kakao소셜로그인 추가

--- src/main/java/hello/golong/domain/member/api/MemberController.java
+++ src/main/java/hello/golong/domain/member/api/MemberController.java
@@ -1,10 +1,49 @@
 package hello.golong.domain.member.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hello.golong.domain.member.application.MemberService;
+import hello.golong.domain.member.dto.MemberDto;
+import hello.golong.domain.member.dto.OauthToken;
+import hello.golong.domain.post.dto.PostDto;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/member")
 public class MemberController {
     //TODO : 컨트롤러 작성 및 컨트롤러 테스트
+
+    @Autowired
+    private final MemberService memberService;
+
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/{sns_email}")
+    public ResponseEntity<MemberDto> getMember(@PathVariable("sns_email") String sns_email) {
+        return ResponseEntity.ok().body(memberService.findMemberBySnsEmail(sns_email));
+    }
+    @GetMapping("/oauth/token")
+    public ResponseEntity getLogin(@RequestParam("code")String code){
+        System.out.println(code);
+        // 넘어온 인가 코드를 통해 access_token 발급
+        OauthToken oauthToken = memberService.getAccessToken(code);
+
+        // 발급 받은 accessToken 으로 카카오 회원 정보 DB 저장
+        String jwtToken = memberService.SaveUserAndGetToken(oauthToken.getAccess_token());
+
+        HttpHeaders headers = new HttpHeaders();
+//        headers.add(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+
+        return ResponseEntity.ok().headers(headers).body("success");
+    }
+
+
+
 }
